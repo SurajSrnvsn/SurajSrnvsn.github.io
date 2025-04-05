1dc0a5c49d9a76cc0408df2805bce68ebf71498c
“Suraj <surajsrnvsn@gmail.com>
2025-04-05 07:18:40 +0530

    chore: Update build files

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -30,7 +31,9 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body'
     })
   ]
 }; 
