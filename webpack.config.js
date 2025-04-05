4ecb0f6243bfe2a469ca58b698dfb5e00936ab59
“Suraj <surajsrnvsn@gmail.com>
2025-04-05 07:24:27 +0530

    chore: Update build files

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/SurajSrnvsn.github.io/'
+    publicPath: './'
   },
   module: {
     rules: [
